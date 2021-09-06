22bdd4553d316335d88c5b9ecd37193d8e637d01
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-09-06 11:33:08 -0500

    created Blog model

---
+++ models/Blog.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+//create our Blog Model.
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blog",
+  }
+);
+
+module.exports = Blog;
