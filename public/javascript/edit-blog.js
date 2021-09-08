a9c7682eca7bf953efc6fe1c6849720fd773b0c8
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-09-07 21:43:54 -0500

    fixed edit blog.js so that the blog edited content is now being saved

--- public/javascript/edit-blog.js
+++ public/javascript/edit-blog.js
@@ -2,6 +2,10 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="blog-title"]').value.trim();
+  const content = document
+    .querySelector('input[name="blog-content"]')
+    .value.trim();
+
   const id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
@@ -9,6 +13,7 @@ async function editFormHandler(event) {
     method: "PUT",
     body: JSON.stringify({
       title,
+      content,
     }),
     headers: {
       "Content-Type": "application/json",
